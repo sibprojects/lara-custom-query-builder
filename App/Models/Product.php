017b6e434cb8c09a95799dbea6f666a513e5b70f
siberian <siberian@sibprojects.com>
2024-10-04 02:38:22 +0100

    Add Product

---
+++ App/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Builders\ProductBuilder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ratings(): HasMany
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function newEloquentBuilder($query): ProductBuilder
+    {
+        return new ProductBuilder($query);
+    }
+}
